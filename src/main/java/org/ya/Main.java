5124f4310a83d4ab99765bdc6d57a518b48425c4
AliyaBlacksmiths <ms.al.kuznetsova@mail.ru>
2023-06-21 11:37:17 +0300

    First commit

---
+++ src/main/java/org/ya/Main.java
@@ -0,0 +1,14 @@
+package org.ya;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        System.out.println(1234 / 1000);//первое
+//        System.out.println(1234 / 100);//первые два
+//        System.out.println((1234 / 100) % 10);//второе число
+//        System.out.println((1234 / 10) % 100);//середина два
+//        System.out.println(1234 % 100);//последние два
+//        System.out.println((1234 % 100)/10);//последние два
+//        System.out.println(1234 % 10);//последнее
+    }
+}
---
+++ src/test/java/org/ya/HomeWorkTests.java
@@ -0,0 +1,11 @@
+package org.ya;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeWorkTests {
+
+    @Test
+    void test() {
+        //TODO
+    }
+}
